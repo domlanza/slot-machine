e59e293824b0329d9dafe9ae3f298c1a04556e70
Zachary Goncalves <zach.j.goncalves@gmail.com>
2018-10-18 14:38:00 -0400

    Implement random number generator

--- SlotMachine/Classes/RNGen.cs
+++ SlotMachine/Classes/RNGen.cs
@@ -8,5 +8,18 @@ namespace SlotMachine.Classes
 {
     class RNGen
     {
+        private int[] pictureNum = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int result;
+        Random gen;
+
+        public RNGen()
+        {
+            gen = new Random();
+        }
+
+        public int pickRandomNumber()
+        {
+            return result = gen.Next(0, pictureNum.Length);        
+        }
     }
 }

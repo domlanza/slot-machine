e186e72e6db0e52cc0c76a7eb6662d8afa4ffc3b
Zachary Goncalves <zach.j.goncalves@gmail.com>
2018-10-18 15:02:48 -0400

    Implement basic functionality for frmGameEntry

--- SlotMachine/Forms/frmGameEntry.Designer.cs
+++ SlotMachine/Forms/frmGameEntry.Designer.cs
@@ -40,9 +40,9 @@
             // 
             this.lblWelcome.AutoSize = true;
             this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWelcome.Location = new System.Drawing.Point(239, 71);
+            this.lblWelcome.Location = new System.Drawing.Point(212, 57);
             this.lblWelcome.Name = "lblWelcome";
-            this.lblWelcome.Size = new System.Drawing.Size(433, 29);
+            this.lblWelcome.Size = new System.Drawing.Size(357, 25);
             this.lblWelcome.TabIndex = 0;
             this.lblWelcome.Text = "Welcome to the Fantastic Slot Machine!";
             // 
@@ -50,60 +50,61 @@
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(177, 138);
+            this.lblName.Location = new System.Drawing.Point(157, 110);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(272, 25);
+            this.lblName.Size = new System.Drawing.Size(226, 20);
             this.lblName.TabIndex = 1;
             this.lblName.Text = "Please enter your name to begin:";
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(466, 138);
-            this.txtName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtName.Location = new System.Drawing.Point(414, 110);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(140, 26);
+            this.txtName.Size = new System.Drawing.Size(125, 22);
             this.txtName.TabIndex = 2;
             // 
             // btnEnterName
             // 
-            this.btnEnterName.Location = new System.Drawing.Point(633, 137);
-            this.btnEnterName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnEnterName.Location = new System.Drawing.Point(563, 110);
             this.btnEnterName.Name = "btnEnterName";
-            this.btnEnterName.Size = new System.Drawing.Size(84, 29);
+            this.btnEnterName.Size = new System.Drawing.Size(75, 23);
             this.btnEnterName.TabIndex = 3;
             this.btnEnterName.Text = "Enter";
             this.btnEnterName.UseVisualStyleBackColor = true;
+            this.btnEnterName.Click += new System.EventHandler(this.btnEnterName_Click);
             // 
             // btnEnterGame
             // 
-            this.btnEnterGame.Location = new System.Drawing.Point(371, 398);
+            this.btnEnterGame.Enabled = false;
+            this.btnEnterGame.Location = new System.Drawing.Point(330, 318);
+            this.btnEnterGame.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnEnterGame.Name = "btnEnterGame";
-            this.btnEnterGame.Size = new System.Drawing.Size(181, 64);
+            this.btnEnterGame.Size = new System.Drawing.Size(161, 51);
             this.btnEnterGame.TabIndex = 4;
             this.btnEnterGame.Text = "Play";
             this.btnEnterGame.UseVisualStyleBackColor = true;
+            this.btnEnterGame.Click += new System.EventHandler(this.btnEnterGame_Click);
             // 
             // lblInstructions
             // 
             this.lblInstructions.AutoSize = true;
-            this.lblInstructions.Location = new System.Drawing.Point(182, 228);
+            this.lblInstructions.Location = new System.Drawing.Point(162, 182);
             this.lblInstructions.Name = "lblInstructions";
-            this.lblInstructions.Size = new System.Drawing.Size(238, 20);
+            this.lblInstructions.Size = new System.Drawing.Size(212, 17);
             this.lblInstructions.TabIndex = 5;
             this.lblInstructions.Text = "Instructions and tips will go here!";
             // 
             // frmGameEntry
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(936, 607);
+            this.ClientSize = new System.Drawing.Size(832, 486);
             this.Controls.Add(this.lblInstructions);
             this.Controls.Add(this.btnEnterGame);
             this.Controls.Add(this.btnEnterName);
             this.Controls.Add(this.txtName);
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.lblWelcome);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmGameEntry";
--- SlotMachine/Forms/frmGameEntry.cs
+++ SlotMachine/Forms/frmGameEntry.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SlotMachine.Classes; 
 
 namespace SlotMachine.Forms
 {
@@ -15,6 +16,37 @@ namespace SlotMachine.Forms
         public frmGameEntry()
         {
             InitializeComponent();
+        }
+
+        private void btnEnterName_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = txtName.Text;
+                if (string.IsNullOrEmpty(name))
+                {
+                    MessageBox.Show("Please enter a valid name.", "Error");
+                    txtName.Text = "";
+                    txtName.Focus();
+                }
+                else
+                {
+                    Player player = new Player(name);
+                    btnEnterGame.Enabled = true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Please enter a valid name.", "Error");
+                txtName.Text = "";
+                txtName.Focus();
+            }
+        }
+
+        private void btnEnterGame_Click(object sender, EventArgs e)
+        {
+            Form frmSlots = new frmSlots();
+            frmSlots.Show();
         }
     }
 }

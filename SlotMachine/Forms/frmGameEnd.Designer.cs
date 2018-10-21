8edb75a1d6be1d7862b9e653b04e1e76e47e7768
Zach Goncalves <zach.j.goncalves@gmail.com>
2018-10-21 14:23:56 -0400

    Implement frmGameEnd

---
+++ SlotMachine/Forms/frmGameEnd.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SlotMachine.Forms
+{
+    partial class frmGameEnd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.btnViewScores = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.lblEndingMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.Location = new System.Drawing.Point(43, 163);
+            this.btnPlayAgain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(113, 44);
+            this.btnPlayAgain.TabIndex = 0;
+            this.btnPlayAgain.Text = "Play Again";
+            this.btnPlayAgain.UseVisualStyleBackColor = true;
+            // 
+            // btnViewScores
+            // 
+            this.btnViewScores.Location = new System.Drawing.Point(199, 163);
+            this.btnViewScores.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnViewScores.Name = "btnViewScores";
+            this.btnViewScores.Size = new System.Drawing.Size(113, 44);
+            this.btnViewScores.TabIndex = 1;
+            this.btnViewScores.Text = "View Scoreboard";
+            this.btnViewScores.UseVisualStyleBackColor = true;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(139, 233);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 28);
+            this.btnQuit.TabIndex = 2;
+            this.btnQuit.Text = "Exit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            // 
+            // lblEndingMessage
+            // 
+            this.lblEndingMessage.AutoSize = true;
+            this.lblEndingMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEndingMessage.Location = new System.Drawing.Point(48, 75);
+            this.lblEndingMessage.Name = "lblEndingMessage";
+            this.lblEndingMessage.Size = new System.Drawing.Size(261, 20);
+            this.lblEndingMessage.TabIndex = 3;
+            this.lblEndingMessage.Text = "Congratulations, you won $650!";
+            // 
+            // frmGameEnd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 281);
+            this.Controls.Add(this.lblEndingMessage);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnViewScores);
+            this.Controls.Add(this.btnPlayAgain);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmGameEnd";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game Over";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlayAgain;
+        private System.Windows.Forms.Button btnViewScores;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label lblEndingMessage;
+    }
+}

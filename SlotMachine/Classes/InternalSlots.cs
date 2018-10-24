30c5fc55c1d22bc4f5527f9df0fdb9e6f06bd006
Zach Goncalves <zach.j.goncalves@gmail.com>
2018-10-23 21:01:07 -0400

    (WIP) Implement InternalSlots class

---
+++ SlotMachine/Classes/InternalSlots.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlotMachine.Classes
+{
+    class InternalSlots
+    {
+        private int coinNum = 0;
+        private int reelResult = 0;
+
+        public int getReelResult(int reelOneResult, int reelTwoResult, int realThreeResult)
+        {
+            return reelResult = Convert.ToInt32(reelOneResult.ToString() + reelTwoResult.ToString() + realThreeResult.ToString());
+        }
+
+        public int spinReel()
+        {
+            // Are we using one RNG or multiple? How are we keeping track? 
+            return 0;
+        }
+
+    }
+}

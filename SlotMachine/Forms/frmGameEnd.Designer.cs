d13b791bb667d7ce7e55da64f9b1d76fa01ac1fe
zachgoncalves <zach.j.goncalves@gmail.com>
2018-10-27 22:36:15 -0400

    Add image files to project.

--- SlotMachine/Forms/frmGameEnd.Designer.cs
+++ SlotMachine/Forms/frmGameEnd.Designer.cs
@@ -37,28 +37,29 @@
             // 
             // btnPlayAgain
             // 
-            this.btnPlayAgain.Location = new System.Drawing.Point(64, 251);
+            this.btnPlayAgain.Location = new System.Drawing.Point(43, 163);
+            this.btnPlayAgain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnPlayAgain.Name = "btnPlayAgain";
-            this.btnPlayAgain.Size = new System.Drawing.Size(170, 68);
+            this.btnPlayAgain.Size = new System.Drawing.Size(113, 44);
             this.btnPlayAgain.TabIndex = 0;
             this.btnPlayAgain.Text = "Play Again";
             this.btnPlayAgain.UseVisualStyleBackColor = true;
             // 
             // btnViewScores
             // 
-            this.btnViewScores.Location = new System.Drawing.Point(298, 251);
+            this.btnViewScores.Location = new System.Drawing.Point(199, 163);
+            this.btnViewScores.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnViewScores.Name = "btnViewScores";
-            this.btnViewScores.Size = new System.Drawing.Size(170, 68);
+            this.btnViewScores.Size = new System.Drawing.Size(113, 44);
             this.btnViewScores.TabIndex = 1;
             this.btnViewScores.Text = "View Scoreboard";
             this.btnViewScores.UseVisualStyleBackColor = true;
             // 
             // btnQuit
             // 
-            this.btnQuit.Location = new System.Drawing.Point(208, 358);
-            this.btnQuit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnQuit.Location = new System.Drawing.Point(139, 233);
             this.btnQuit.Name = "btnQuit";
-            this.btnQuit.Size = new System.Drawing.Size(112, 43);
+            this.btnQuit.Size = new System.Drawing.Size(75, 28);
             this.btnQuit.TabIndex = 2;
             this.btnQuit.Text = "Exit";
             this.btnQuit.UseVisualStyleBackColor = true;
@@ -68,10 +69,9 @@
             // 
             this.lblEndingMessage.AutoSize = true;
             this.lblEndingMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEndingMessage.Location = new System.Drawing.Point(120, 73);
-            this.lblEndingMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEndingMessage.Location = new System.Drawing.Point(80, 47);
             this.lblEndingMessage.Name = "lblEndingMessage";
-            this.lblEndingMessage.Size = new System.Drawing.Size(306, 29);
+            this.lblEndingMessage.Size = new System.Drawing.Size(211, 20);
             this.lblEndingMessage.TabIndex = 3;
             this.lblEndingMessage.Text = "Congratulations, you won";
             // 
@@ -79,23 +79,25 @@
             // 
             this.lblWinnings.AutoSize = true;
             this.lblWinnings.Font = new System.Drawing.Font("Malgun Gothic", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWinnings.Location = new System.Drawing.Point(191, 133);
+            this.lblWinnings.Location = new System.Drawing.Point(132, 81);
+            this.lblWinnings.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblWinnings.Name = "lblWinnings";
-            this.lblWinnings.Size = new System.Drawing.Size(129, 60);
+            this.lblWinnings.Size = new System.Drawing.Size(86, 41);
             this.lblWinnings.TabIndex = 4;
             this.lblWinnings.Text = "$650";
             this.lblWinnings.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // frmGameEnd
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(538, 432);
+            this.ClientSize = new System.Drawing.Size(359, 281);
             this.Controls.Add(this.lblWinnings);
             this.Controls.Add(this.lblEndingMessage);
             this.Controls.Add(this.btnQuit);
             this.Controls.Add(this.btnViewScores);
             this.Controls.Add(this.btnPlayAgain);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmGameEnd";

3f9b6ef6a62fb67e1099b16736801a64c149c1a7
zachgoncalves <zach.j.goncalves@gmail.com>
2018-10-27 23:43:13 -0400

    Add background to frmSlots

--- SlotMachine/Forms/frmSlots.Designer.cs
+++ SlotMachine/Forms/frmSlots.Designer.cs
@@ -53,10 +53,11 @@
             // 
             // lblGameControls
             // 
+            this.lblGameControls.BackColor = System.Drawing.Color.Transparent;
             this.lblGameControls.Controls.Add(this.btnMaxBet);
             this.lblGameControls.Controls.Add(this.btnSpin);
             this.lblGameControls.Controls.Add(this.btnPlaceBet);
-            this.lblGameControls.Location = new System.Drawing.Point(261, 389);
+            this.lblGameControls.Location = new System.Drawing.Point(261, 444);
             this.lblGameControls.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.lblGameControls.Name = "lblGameControls";
             this.lblGameControls.Size = new System.Drawing.Size(440, 68);
@@ -98,8 +99,8 @@
             // 
             // pbReel1
             // 
-            this.pbReel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pbReel1.Location = new System.Drawing.Point(147, 40);
+            this.pbReel1.BackColor = System.Drawing.Color.Transparent;
+            this.pbReel1.Location = new System.Drawing.Point(94, 69);
             this.pbReel1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.pbReel1.Name = "pbReel1";
             this.pbReel1.Size = new System.Drawing.Size(206, 241);
@@ -108,8 +109,8 @@
             // 
             // pbReel2
             // 
-            this.pbReel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pbReel2.Location = new System.Drawing.Point(378, 40);
+            this.pbReel2.BackColor = System.Drawing.Color.Transparent;
+            this.pbReel2.Location = new System.Drawing.Point(380, 69);
             this.pbReel2.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.pbReel2.Name = "pbReel2";
             this.pbReel2.Size = new System.Drawing.Size(206, 241);
@@ -118,8 +119,8 @@
             // 
             // pbReel3
             // 
-            this.pbReel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pbReel3.Location = new System.Drawing.Point(607, 40);
+            this.pbReel3.BackColor = System.Drawing.Color.Transparent;
+            this.pbReel3.Location = new System.Drawing.Point(664, 69);
             this.pbReel3.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.pbReel3.Name = "pbReel3";
             this.pbReel3.Size = new System.Drawing.Size(206, 241);
@@ -128,8 +129,10 @@
             // 
             // txtBalance
             // 
+            this.txtBalance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
             this.txtBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBalance.Location = new System.Drawing.Point(454, 300);
+            this.txtBalance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtBalance.Location = new System.Drawing.Point(455, 369);
             this.txtBalance.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.txtBalance.Name = "txtBalance";
             this.txtBalance.ReadOnly = true;
@@ -140,8 +143,10 @@
             // lblBalance
             // 
             this.lblBalance.AutoSize = true;
+            this.lblBalance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
             this.lblBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBalance.Location = new System.Drawing.Point(355, 322);
+            this.lblBalance.ForeColor = System.Drawing.Color.White;
+            this.lblBalance.Location = new System.Drawing.Point(356, 391);
             this.lblBalance.Name = "lblBalance";
             this.lblBalance.Size = new System.Drawing.Size(91, 20);
             this.lblBalance.TabIndex = 5;
@@ -149,8 +154,10 @@
             // 
             // txtWinnings
             // 
+            this.txtWinnings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
             this.txtWinnings.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtWinnings.Location = new System.Drawing.Point(189, 300);
+            this.txtWinnings.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtWinnings.Location = new System.Drawing.Point(190, 369);
             this.txtWinnings.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.txtWinnings.Name = "txtWinnings";
             this.txtWinnings.ReadOnly = true;
@@ -161,8 +168,10 @@
             // lblWinnings
             // 
             this.lblWinnings.AutoSize = true;
+            this.lblWinnings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
             this.lblWinnings.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWinnings.Location = new System.Drawing.Point(75, 322);
+            this.lblWinnings.ForeColor = System.Drawing.Color.White;
+            this.lblWinnings.Location = new System.Drawing.Point(76, 391);
             this.lblWinnings.Name = "lblWinnings";
             this.lblWinnings.Size = new System.Drawing.Size(99, 20);
             this.lblWinnings.TabIndex = 7;
@@ -171,8 +180,10 @@
             // lblBet
             // 
             this.lblBet.AutoSize = true;
+            this.lblBet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
             this.lblBet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBet.Location = new System.Drawing.Point(723, 322);
+            this.lblBet.ForeColor = System.Drawing.Color.White;
+            this.lblBet.Location = new System.Drawing.Point(724, 391);
             this.lblBet.Name = "lblBet";
             this.lblBet.Size = new System.Drawing.Size(43, 20);
             this.lblBet.TabIndex = 9;
@@ -180,8 +191,10 @@
             // 
             // txtBet
             // 
+            this.txtBet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
             this.txtBet.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBet.Location = new System.Drawing.Point(776, 300);
+            this.txtBet.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtBet.Location = new System.Drawing.Point(777, 369);
             this.txtBet.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.txtBet.Name = "txtBet";
             this.txtBet.ReadOnly = true;
@@ -191,7 +204,7 @@
             // 
             // btnPayout
             // 
-            this.btnPayout.Location = new System.Drawing.Point(832, 481);
+            this.btnPayout.Location = new System.Drawing.Point(822, 467);
             this.btnPayout.Name = "btnPayout";
             this.btnPayout.Size = new System.Drawing.Size(89, 31);
             this.btnPayout.TabIndex = 10;
@@ -202,7 +215,7 @@
             // test1
             // 
             this.test1.AutoSize = true;
-            this.test1.Location = new System.Drawing.Point(147, 8);
+            this.test1.Location = new System.Drawing.Point(137, 32);
             this.test1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.test1.Name = "test1";
             this.test1.Size = new System.Drawing.Size(38, 13);
@@ -212,7 +225,7 @@
             // test2
             // 
             this.test2.AutoSize = true;
-            this.test2.Location = new System.Drawing.Point(378, 8);
+            this.test2.Location = new System.Drawing.Point(404, 32);
             this.test2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.test2.Name = "test2";
             this.test2.Size = new System.Drawing.Size(38, 13);
@@ -222,7 +235,7 @@
             // test3
             // 
             this.test3.AutoSize = true;
-            this.test3.Location = new System.Drawing.Point(607, 8);
+            this.test3.Location = new System.Drawing.Point(687, 32);
             this.test3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.test3.Name = "test3";
             this.test3.Size = new System.Drawing.Size(38, 13);
@@ -233,6 +246,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SlotMachine.Properties.Resources.formGamebg;
             this.ClientSize = new System.Drawing.Size(949, 537);
             this.Controls.Add(this.test3);
             this.Controls.Add(this.test2);
@@ -248,6 +262,7 @@
             this.Controls.Add(this.pbReel2);
             this.Controls.Add(this.pbReel1);
             this.Controls.Add(this.lblGameControls);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;

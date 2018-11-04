6dcd4978eb18c895579a04cb68afce4c3827942f
Zach Goncalves <zach.j.goncalves@gmail.com>
2018-11-04 17:10:47 -0500

    Add comments to player class

--- SlotMachine/Classes/Player.cs
+++ SlotMachine/Classes/Player.cs
@@ -1,6 +1,6 @@
 ﻿/* Temple University / CIS 3309 / Slot Machine
  * Class: Player 
- * Author: Zachary Goncalves
+ * Authors: Zachary Goncalves & Michael Rodriguez
  */
 
 using System;
@@ -16,43 +16,54 @@ namespace SlotMachine.Classes
         private string playerName;
         private int score = 0;
         private int playerBalance=0;
-
+        
+        // Constructor that sets player name and balance.
+        // Author: Michael Rodriguez
         public void setPlayer(string name, int balance)
         {
             playerName = name;
             playerBalance = Math.Abs(balance);
         }
-
+        // Gets and returns player name.
+        // Author: Zachary Goncalves
         public string getPlayerName()
         {
             return playerName;
         }
-
-		public void setScore(int scoreInput)
+        // Sets player score.
+        // Author: Zachary Goncalves
+        public void setScore(int scoreInput)
 		{
 			score = scoreInput;
 		}
-		public int getBalance()
+        // Gets and returns player balance.
+        // Author: Zachary Goncalves
+        public int getBalance()
 		{
 			return playerBalance;
 		}
-
+        // Gets and returns player score.
+        // Author: Zachary Goncalves
         public int getScore()
         {
             return score;
         }
-
+        // Caclculates score from score and player balance.
+        // Author: Michael Rodriguez
         public int calculateScore()
         {
             return score = score - playerBalance;
         }
-
+        // Resets player attributes.
+        // Author: Zachary Goncalves
         public void resetPlayer()
         {
             score = 0;
             playerBalance = 0;
             playerName = "";
         }
+        // Formats player name and score into string.
+        // Author: Zachary Goncalves
         public string preparePlayerScore()
         {
             string finalScore = playerName + ", " + score;

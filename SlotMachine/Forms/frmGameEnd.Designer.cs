b509d016833883433a0be4da88303c96745807a9
Zach Goncalves <zach.j.goncalves@gmail.com>
2018-11-04 15:26:41 -0500

    Add background to last form.

--- SlotMachine/Forms/frmGameEnd.Designer.cs
+++ SlotMachine/Forms/frmGameEnd.Designer.cs
@@ -37,10 +37,10 @@
             // 
             // btnPlayAgain
             // 
-            this.btnPlayAgain.Location = new System.Drawing.Point(57, 201);
+            this.btnPlayAgain.Location = new System.Drawing.Point(78, 251);
             this.btnPlayAgain.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnPlayAgain.Name = "btnPlayAgain";
-            this.btnPlayAgain.Size = new System.Drawing.Size(151, 54);
+            this.btnPlayAgain.Size = new System.Drawing.Size(170, 68);
             this.btnPlayAgain.TabIndex = 0;
             this.btnPlayAgain.Text = "Play Again";
             this.btnPlayAgain.UseVisualStyleBackColor = true;
@@ -48,10 +48,10 @@
             // 
             // btnViewScores
             // 
-            this.btnViewScores.Location = new System.Drawing.Point(265, 201);
+            this.btnViewScores.Location = new System.Drawing.Point(312, 251);
             this.btnViewScores.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnViewScores.Name = "btnViewScores";
-            this.btnViewScores.Size = new System.Drawing.Size(151, 54);
+            this.btnViewScores.Size = new System.Drawing.Size(170, 68);
             this.btnViewScores.TabIndex = 1;
             this.btnViewScores.Text = "View Scoreboard";
             this.btnViewScores.UseVisualStyleBackColor = true;
@@ -59,10 +59,10 @@
             // 
             // btnQuit
             // 
-            this.btnQuit.Location = new System.Drawing.Point(185, 287);
-            this.btnQuit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnQuit.Location = new System.Drawing.Point(215, 364);
+            this.btnQuit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnQuit.Name = "btnQuit";
-            this.btnQuit.Size = new System.Drawing.Size(100, 34);
+            this.btnQuit.Size = new System.Drawing.Size(112, 42);
             this.btnQuit.TabIndex = 2;
             this.btnQuit.Text = "Exit";
             this.btnQuit.UseVisualStyleBackColor = true;
@@ -71,30 +71,33 @@
             // lblEndingMessage
             // 
             this.lblEndingMessage.AutoSize = true;
+            this.lblEndingMessage.BackColor = System.Drawing.Color.Transparent;
             this.lblEndingMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEndingMessage.Location = new System.Drawing.Point(107, 58);
+            this.lblEndingMessage.Location = new System.Drawing.Point(130, 73);
             this.lblEndingMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblEndingMessage.Name = "lblEndingMessage";
-            this.lblEndingMessage.Size = new System.Drawing.Size(256, 25);
+            this.lblEndingMessage.Size = new System.Drawing.Size(306, 29);
             this.lblEndingMessage.TabIndex = 3;
             this.lblEndingMessage.Text = "Congratulations, you won";
             // 
             // lblWinnings
             // 
             this.lblWinnings.AutoSize = true;
+            this.lblWinnings.BackColor = System.Drawing.Color.Transparent;
             this.lblWinnings.Font = new System.Drawing.Font("Malgun Gothic", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWinnings.Location = new System.Drawing.Point(176, 100);
+            this.lblWinnings.Location = new System.Drawing.Point(216, 133);
             this.lblWinnings.Name = "lblWinnings";
-            this.lblWinnings.Size = new System.Drawing.Size(106, 50);
+            this.lblWinnings.Size = new System.Drawing.Size(129, 60);
             this.lblWinnings.TabIndex = 4;
             this.lblWinnings.Text = "$650";
             this.lblWinnings.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // frmGameEnd
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(479, 346);
+            this.BackgroundImage = global::SlotMachine.Properties.Resources.frmgameend;
+            this.ClientSize = new System.Drawing.Size(560, 485);
             this.Controls.Add(this.lblWinnings);
             this.Controls.Add(this.lblEndingMessage);
             this.Controls.Add(this.btnQuit);
--- SlotMachine/Properties/Resources.Designer.cs
+++ SlotMachine/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace SlotMachine.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap frmgameend {
+            get {
+                object obj = ResourceManager.GetObject("frmgameend", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap green_t {
             get {
                 object obj = ResourceManager.GetObject("green_t", resourceCulture);
